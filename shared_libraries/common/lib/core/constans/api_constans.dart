1d54c55d2242261e57454d12a872e821fc652509
Bintaaaa <bijantium.yt.32@gmail.com>
2024-11-01 17:29:53 +0700

    feat: set up common module

---
+++ shared_libraries/common/lib/core/constans/api_constans.dart
@@ -0,0 +1,9 @@
+class ApiConstans {
+  ApiConstans();
+
+  String get baseUrl => "https://reqres.in/api/";
+
+  //[INFO]  Get all about users
+  String get users => "users";
+  String get login => "login";
+}
